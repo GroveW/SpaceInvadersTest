c508c562e8ad8791dc923b8ab4cebc25573dc657
GroveW <qswdw3ed3@gmail.com>
2021-02-06 15:52:48 +0100

    Add shooting

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -11,9 +11,33 @@ public class Player : MonoBehaviour
 
     private int direction = 0;
 
+    [SerializeField]
+    private float fireInterval = 2.0f;
+    private float fireStamp = 0.0f;
+
+    [SerializeField]
+    private GameObject bullet;
+
+    private float bulletSpeed = 2.0f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Time.time - fireStamp >= fireInterval)
+        {
+            fireStamp = Time.time;
+
+            Instantiate(bullet, transform.position, Quaternion.identity).GetComponent<Rigidbody2D>().velocity = Vector2.up * bulletSpeed;
+        }
     }
 
     void FixedUpdate()
